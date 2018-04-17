4d616a174d2c27010c86fe08c4a7497b7bae948e
Hou Junjie <houjunjie@qiniu.com>
2018-04-17 23:28:13 +0800

    base docker scripts

---
+++ docker/build.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+docker build -t ps-lite:test -f dockerfile ..
+
+if [ "$1" = "--push" ]; then
+    VERSION=`date -u '+%Y%m%d'`-`git rev-parse --short HEAD`
+    docker tag ps-lite:test reg-xs.qiniu.io/atlab/ps-lite:$VERSION
+    docker push reg-xs.qiniu.io/atlab/ps-lite:$VERSION
+fi
